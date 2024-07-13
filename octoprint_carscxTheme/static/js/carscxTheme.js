1c032a3b886327826293ad64603f44129162da43
Carlos Santos <karscx@gmail.com>
2024-07-13 01:40:50 -0300

    Fix

--- octoprint_carscxTheme/static/js/carscxTheme.js
+++ octoprint_carscxTheme/static/js/carscxTheme.js
@@ -8,9 +8,22 @@ $(function() {
     function CarscxthemeViewModel(parameters) {
         var self = this;
 
-        // Assign the injected parameters, e.g.:
-        // self.loginStateViewModel = parameters[0];
-        // self.settingsViewModel = parameters[1];
+        // Inject the Google Fonts link tags into the head
+        var link1 = document.createElement('link');
+        link1.rel = 'preconnect';
+        link1.href = 'https://fonts.googleapis.com';
+        document.head.appendChild(link1);
+
+        var link2 = document.createElement('link');
+        link2.rel = 'preconnect';
+        link2.href = 'https://fonts.gstatic.com';
+        link2.crossOrigin = 'anonymous';
+        document.head.appendChild(link2);
+
+        var link3 = document.createElement('link');
+        link3.rel = 'stylesheet';
+        link3.href = 'https://fonts.googleapis.com/css2?family=Nunito+Sans:opsz,wght@6..12,200&display=swap';
+        document.head.appendChild(link3);
 
         // Add the class 'carscxTheme' to the body element
         $('body').addClass('carscxTheme');
@@ -18,7 +31,7 @@ $(function() {
         // TODO: Implement your plugin's view model here.
     }
 
-    /* View model class, parameters for constructor, container to bind to
+    /* view model class, parameters for constructor, container to bind to
      * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
      * and a full list of the available options.
      */
